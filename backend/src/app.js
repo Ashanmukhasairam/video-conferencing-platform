ea7b8c565126c2caa78e6c3626d8e5819e1a2464
Ashanmukhasairam <shanmukhaagnihotram@gmail.com>
2025-04-24 17:47:02 +0530

    Basic Socket.io connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,29 @@
-import express from 'express';
-
-import {createServer} from 'node:http';
-
-import {Server} from "socket.io";
-
-import mongoose from 'mongoose'; 
-
-import cors from 'cors';
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv';
+import {connectToSocket} from "./controllers/socketManager.js";
+dotenv.config({ path: '../.env' });
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+
+app.get("/home", (req, res) => {
+  res.json({ message: "Hello from the server!" });
 });
 
-app.get('/home', (req, res) => {
-    res.send('Hello World!');
-});
-
-const start =async () =>{
-
-    app.listen(8000,()=>{
-        console.log('Server is running on port 8000');
-    });
-}
+const start = async () => {
+  const connectionDB =  await  mongoose.connect(process.env.MONGO_URL)
+  console.log(`MonggoDB connected: ${connectionDB.connection.host}`);
+    
+  server.listen(app.get("port"), () => {
+    console.log(`Server is running on port ${app.get("port")}`);
+  });
+};
 
 start();
