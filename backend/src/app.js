57b3f09e789b8157ed308107b0e0f11dcf72a670
Ashanmukhasairam <shanmukhaagnihotram@gmail.com>
2025-04-28 21:19:06 +0530

    mongodb connected successfully

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,37 @@
 import express from "express";
 import { createServer } from "node:http";
+
 import { Server } from "socket.io";
+
 import mongoose from "mongoose";
+
 import cors from "cors";
-import dotenv from 'dotenv';
-import {connectToSocket} from "./controllers/socketManager.js";
-dotenv.config({ path: '../.env' });
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+
+import userRoutes from "./routes/user.routes.js";
+import { connectToSocket } from "./controllers/socketManager.js";
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/api/v1/users",userRoutes);
+
 
 app.get("/home", (req, res) => {
   res.json({ message: "Hello from the server!" });
 });
 
 const start = async () => {
-  const connectionDB =  await  mongoose.connect(process.env.MONGO_URL)
+  const connectionDB = await mongoose.connect(process.env.MONGO_URL);
   console.log(`MonggoDB connected: ${connectionDB.connection.host}`);
-    
+
   server.listen(app.get("port"), () => {
     console.log(`Server is running on port ${app.get("port")}`);
   });
